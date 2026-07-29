44ac7f4a1a5e0cbc84cb2a2a3988722e39185615
ronaldopeixotonet <ronaldopeixoto.net@gmail.com>
2026-07-29 15:55:28 -0300

    Adiciona scope offline_access para receber refresh_token

--- models/meli_util.py
+++ models/meli_util.py
@@ -607,7 +607,8 @@ class MeliApiNoSDK:
             'client_id': self.client_id,
             'response_type': 'code',
             'redirect_uri': self.redirect_uri,
-            'state': random_id
+            'state': random_id,
+            'scope': 'offline_access'
         }
         url = self.AUTH_URL + '?' + urlencode(params)
         return url
@@ -968,7 +969,7 @@ if _versions.MELI_SDK_AVAILABLE and _meli_sdk and _ApiClient:
             if redirect_URI:
                 self.redirect_uri = redirect_URI
             random_id = str(now)
-            params = {'client_id': self.client_id, 'response_type': 'code', 'redirect_uri': self.redirect_uri, 'state': random_id}
+            params = {'client_id': self.client_id, 'response_type': 'code', 'redirect_uri': self.redirect_uri, 'state': random_id, 'scope': 'offline_access'}
             return self.AUTH_URL + '?' + urlencode(params)
 
         def redirect_login(self):
